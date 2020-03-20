66d7e68cde66203a00a1474c44b80c4db2f22101
Matti Eiden <matti.eiden@bitfactor.fi>
2020-03-20 02:42:17 +0200

    Integrate markdown to topics

--- src/views/topic-view.js
+++ src/views/topic-view.js
@@ -5,68 +5,76 @@ import {observer} from "mobx-react";
 import RegistryLink from "../components/registry-link";
 import Registry from "../components/registry";
 import DatasheetLink from "../components/datasheet-link";
+import MarkdownIt from 'markdown-it';
+import * as hljs from "highlight.js";
+import "highlight.js/styles/dracula.css";
+import ReactHtmlParser from 'react-html-parser';
 
-const rePlacer = new RegExp("<(\\w+):([\\w.]+)>");
+const md = new MarkdownIt({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (__) {
+      }
+    }
+    return ''; // use external default escaping
+  },
+  html: true,
+});
 
 const formatText = function formatText(text) {
-    let content = [text ? text : ""];
-    let references = [];
-    let match;
-    while ((match = content[content.length - 1].match(rePlacer))) {
-        const [matchSegment, key, value] = match;
-        const segment = content[content.length - 1];
-        const before = segment.slice(0, match.index);
-        const after = segment.slice(match.index + matchSegment.length, segment.length);
-        let link = `(??? ${key} ???)`;
-        let reference = null;
-        if (key === "REG") {
-            const [registry, offset, field] = value.split(".");
-            link = <RegistryLink registry={registry} offset={offset} field={field}/>;
-            reference = <Registry registry={registry} offset={offset} field={field}/>;
-        } else if (key === "REF") {
-            link = <DatasheetLink page={value}/>
-        }
-        content[content.length - 1] = before;
-        content.push(link);
-        content.push(after);
-        references.push(reference);
+  const references = [];
+  const html = md.render(text);
+  const jsx = ReactHtmlParser(html, {
+    transform: (node, index) => {
+      if (node.parent && node.parent.name === 'reg' && node.type === 'text') {
+        const [registry, offset, field] = node.data.split(".");
+        references.push(<Registry registry={registry} offset={offset} field={field}/>);
+        return <RegistryLink registry={registry} offset={offset} field={field}/>;
+      } else if (node.parent && node.parent.name === 'ref' && node.type === 'text') {
+        return <DatasheetLink page={node.data}/>
+      }
+      return undefined
     }
-    content.push(
-        <div className="references">
-            {references}
-        </div>
-    );
-
-    return content
+  });
+  return (
+    <React.Fragment>
+      {jsx}
+      <div className="references">
+        {references}
+      </div>
+    </React.Fragment>
+  );
 };
 
 const TopicView = observer(function TopicView(props) {
-    const { activeGroup } = store;
-    let formatted = null;
-    if (activeGroup) {
-        const topic = store.getTopic(activeGroup);
-        formatted = topic ? (
-            <React.Fragment>
-                <h3>{topic.title}</h3>
-                <div className="topic-text">{formatText(topic.text)}</div>
-            </React.Fragment>
-        ) : <span>Topic '{activeGroup}' not found</span>
-    }
-    if (!formatted) {
-        const primaryHighlightGroup = store.primaryHighlightGroup;
-        const topic = store.getTopic(primaryHighlightGroup);
-        formatted = topic ? <h3>{topic.title}</h3> : null;
-    }
-    return (
-        <div
-            className="topic-view"
-            onClick={ e => e.stopPropagation() }
-        >
-            <div>
-                {formatted}
-            </div>
-        </div>
-    )
+  const {activeGroup} = store;
+  let formatted = null;
+  if (activeGroup) {
+    const topic = store.getTopic(activeGroup);
+    formatted = topic ? (
+      <React.Fragment>
+        <h3>{topic.title}</h3>
+        <div className="topic-text">{formatText(topic.text)}</div>
+      </React.Fragment>
+    ) : <span>Topic '{activeGroup}' not found</span>
+  }
+  if (!formatted) {
+    const primaryHighlightGroup = store.primaryHighlightGroup;
+    const topic = store.getTopic(primaryHighlightGroup);
+    formatted = topic ? <h3>{topic.title}</h3> : null;
+  }
+  return (
+    <div
+      className="topic-view"
+      onClick={e => e.stopPropagation()}
+    >
+      <div>
+        {formatted}
+      </div>
+    </div>
+  )
 });
 
 export default TopicView;
